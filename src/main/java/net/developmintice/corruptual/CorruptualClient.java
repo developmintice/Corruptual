d90d97c50f6ff02776a54341609aafe2f68175f9
developmintice <straubreychocomint@proton.me>
2025-05-28 05:25:18 +0100

    Initial commit

---
+++ src/main/java/net/developmintice/corruptual/CorruptualClient.java
@@ -0,0 +1,10 @@
+package net.developmintice.corruptual;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CorruptualClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
